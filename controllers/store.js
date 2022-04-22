ae844c04c3c301d1dce63cbfdbbd5d46a2f8db62
Nathan Yi <nathanyi112@gmail.com>
2022-04-21 22:05:57 -0500

    adds restful routes

--- controllers/store.js
+++ controllers/store.js
@@ -2,11 +2,53 @@ const express = require("express")
 const Club = require("../models/club.js")
 const storeRouter = express.Router()
 
+
+// Index Route
 storeRouter.get("/", (req, res) => {
-    res.send("working")
+    res.render("index.ejs")
 })
 
+// New Route
+storeRouter.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
 
+// Delete Route
+storeRouter.delete("/clubs/:id", (req, res) => {
+    Club.findByIdAndDelete(req.params.id, (err, deletedClub) => {
+        res.redirect("/store/clubs")
+    })
+})
 
+// Update Route
+storeRouter.put("/clubs/:id", (req, res) => {
+    Club.findByIdAndUpdate(req.params.id, req.body, (err, updatedClub) => {
+        if(err) console.log(err)
+        res.redirect(`/products/${req.params.id}`)
+    })
+})
+// Create Route
+storeRouter.post("/", (req, res) => {
+    Club.create(req.body, (err, createdClub) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else {
+            res.redirect("/store/clubs")
+        }
+    })
+})
+
+// Edit Route
+storeRouter.get("/clubs/:id/edit", (req, res) => {
+    Club.findById(req.params.id, (err, club) => {
+        res.render("edit.ejs", { club })
+    })
+})
+
+// Show Route
+storeRouter.get("/clubs", (req, res) => {
+    res.render("show.ejs", { club })
+})
 
 module.exports = storeRouter
