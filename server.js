fcce86ed345424487f72c472deb723497a412b44
Nathan Yi <nathanyi112@gmail.com>
2022-04-21 21:38:15 -0500

    adds DB connection

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+
+// Mongoose Connection
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongoose connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`You are listening on Port ${PORT}`)
+})
